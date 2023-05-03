72f2389df677ded53e76cc0df852e8ab60e55c52
Brian Ward <bward@flatironinstitute.org>
2023-05-03 09:56:49 -0400

    Unvendor Eigen, Sundials, and Boost on *nix

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,10 +6,26 @@ cp -r . $PREFIX/bin/cmdstan
 
 cd $PREFIX/bin/cmdstan
 
+# remove old libraries
+rm -rf stan/lib/stan_math/lib/*
+
+# TBB setup
 echo "TBB_CXX_TYPE=${c_compiler}"  >> make/local
 echo "TBB_INTERFACE_NEW=true" >> make/local
 echo "TBB_INC=${PREFIX}/include/" >> make/local
 echo "TBB_LIB=${PREFIX}/lib/" >> make/local
+
+# Eigen setup
+echo "EIGEN=${PREFIX}/include/eigen3/" >> make/local
+# Boost setup
+echo "BOOST=${PREFIX}/include/boost/" >> make/local
+
+# SUNDIALS setup
+echo "INC_SUNDIALS= -I ${PREFIX}/include/"  >> make/local
+echo "LDFLAGS_SUNDIALS= -L ${PREFIX}/lib/" >> make/local
+echo "LDLIBS_SUNDIALS= -lsundials_cvodes -lsundials_idas -lsundials_kinsol -lsundials_nvecserial" >> make/local
+echo "SUNDIALS_TARGETS=" >> make/local
+
 echo "PRECOMPILED_HEADERS=false" >> make/local
 
 make print-compiler-flags
