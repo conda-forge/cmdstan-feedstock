6b39b07df9f32c73e079782418d711b8424cde67
Brian Ward <bward@flatironinstitute.org>
2024-06-25 14:49:13 -0400

    Probably wrong: gxx

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -22,8 +22,6 @@ cd %PREFIX%\Library\bin\cmdstan
 if errorlevel 1 exit 1
 
 echo TBB_CXX_TYPE=gcc >> make\local
-echo CXX=x86_64-w64-mingw32-g++ >> make\local
-echo CC=x86_64-w64-mingw32-gcc >> make\local
 if errorlevel 1 exit 1
 
 make print-compiler-flags
