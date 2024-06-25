75463fc5b18a868e9c45ef30e6b5619a580001d4
Brian Ward <bward@flatironinstitute.org>
2024-06-25 17:04:41 -0400

    Try clang post general availability

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -21,24 +21,67 @@ if errorlevel 1 exit 1
 cd %PREFIX%\Library\bin\cmdstan
 if errorlevel 1 exit 1
 
-echo TBB_CXX_TYPE=gcc >> make\local
+set "fwd_slash_prefix=%PREFIX:\=/%"
+
+echo "Setting up make\local"
+
+echo CC=clang >> make\local
+if errorlevel 1 exit 1
+echo CXX=clang++ >> make\local
 if errorlevel 1 exit 1
 
-mingw32-make print-compiler-flags
+::echo LDFLAGS=-nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld -Wl,-defaultlib:%BUILD_PREFIX:\=/%/lib/clang/14.0.3/lib/windows/clang_rt.builtins-x86_64.lib >> make/local
+
+echo "TBB"
+
+:: TBB setup
+echo TBB_CXX_TYPE=clang >> make\local
+if errorlevel 1 exit 1
+echo TBB_INTERFACE_NEW=true >> make\local
+if errorlevel 1 exit 1
+echo TBB_INC=%PREFIX:\=/%/Library/include/ >> make\local
+if errorlevel 1 exit 1
+echo TBB_LIB=%PREFIX:\=/%/Library/lib/ >> make\local
 if errorlevel 1 exit 1
 
-mingw32-make clean-all
+echo PRECOMPILED_HEADERS=false >> make\local
 if errorlevel 1 exit 1
 
-mingw32-make build -j%CPU_COUNT%
+type make\local
 if errorlevel 1 exit 1
+
+make build -j1
+if errorlevel 1 exit 1
+
+::mingw32-make print-compiler-flags
+::if errorlevel 1 exit 1
+
+::mingw32-make clean-all
+::if errorlevel 1 exit 1
+
+echo "Attempting to build"
+
+:: for now, just try to build bernoulli
+
+clang++ -I %fwd_slash_prefix%/Library/include/ -I src -I stan/src -I stan/lib/rapidjson_1.1.0/ -I lib/CLI11-1.9.1/ -I stan/lib/stan_math/ -I stan/lib/stan_math/lib/eigen_3.4.0 -I stan/lib/stan_math/lib/boost_1.78.0 -std=c++1y -O3 -D_REENTRANT -DBOOST_DISABLE_ASSERTS -D_BOOST_LGAMMA -DTBB_INTERFACE_NEW -Wno-sign-compare -Wno-deprecated-builtins -Wno-ignored-attributes -c -o src/cmdstan/main.o src/cmdstan/main.cpp
+
+.\bin\stanc.exe  --o=examples/bernoulli/bernoulli.cpp examples/bernoulli/bernoulli.stan
+
+clang++ -I %fwd_slash_prefix%/Library/include/ -I src -I stan/src -I stan/lib/rapidjson_1.1.0/ -I lib/CLI11-1.9.1/ -I stan/lib/stan_math/ -I stan/lib/stan_math/lib/eigen_3.4.0 -I stan/lib/stan_math/lib/boost_1.78.0 -std=c++1y -O3 -D_REENTRANT -DBOOST_DISABLE_ASSERTS -D_BOOST_LGAMMA -DTBB_INTERFACE_NEW -Wno-sign-compare -Wno-deprecated-builtins -Wno-ignored-attributes -c -o examples/bernoulli/bernoulli.o examples/bernoulli/bernoulli.cpp
+
+
+clang++ -I %fwd_slash_prefix%/Library/include/ -I src -I stan/src -I stan/lib/rapidjson_1.1.0/ -I lib/CLI11-1.9.1/ -I stan/lib/stan_math/ -I stan/lib/stan_math/lib/eigen_3.4.0 -I stan/lib/stan_math/lib/boost_1.78.0 -std=c++1y -O3 -D_REENTRANT -DBOOST_DISABLE_ASSERTS -D_BOOST_LGAMMA -DTBB_INTERFACE_NEW -Wno-sign-compare -Wno-deprecated-builtins -Wno-ignored-attributes  examples/bernoulli/bernoulli.o src/cmdstan/main.o -Wl",/LIBPATH:%PREFIX%\Library\lib\" -ltbb -lsundials_nvecserial -lsundials_cvodes -lsundials_idas -lsundials_kinsol -o examples/bernoulli/bernoulli.exe
+
+
+examples\bernoulli\bernoulli.exe sample data file=examples/bernoulli/bernoulli.data.json
+
+
+::mingw32-make build -j%CPU_COUNT%
+::if errorlevel 1 exit 1
 :: also compile threads header
-mingw32-make build -j%CPU_COUNT% STAN_THREADS=TRUE
-if errorlevel 1 exit 1
+::mingw32-make build -j%CPU_COUNT% STAN_THREADS=TRUE
+::if errorlevel 1 exit 1
 
 :: not read-only
 attrib -R /S
-if errorlevel 1 exit 1
-
-copy stan\lib\stan_math\lib\tbb\tbb.dll ..
 if errorlevel 1 exit 1
