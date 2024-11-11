73cd7a7b2df105fb19f81efc260a0ec44e1e965c
Brian Ward <bward@flatironinstitute.org>
2024-11-11 15:42:48 -0500

    Try swapping link order

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -4,6 +4,11 @@ echo %CMDSTAN%
 cd %PREFIX%\Library\bin\cmdstan
 if errorlevel 1 exit 1
 
+type make\local
+if errorlevel 1 exit 1
+
+make print-compiler-flags
+if errorlevel 1 exit 1
 
 :: test binaries
 bin\stanc.exe --help
