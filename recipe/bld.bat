4b2b9d0ad4cdf47de80b0fd401ce37d9dd272a80
Brian Ward <bward@flatironinstitute.org>
2024-11-11 16:28:58 -0500

    Fix LD name

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -35,7 +35,7 @@ echo CC=clang >> make\local
 if errorlevel 1 exit 1
 echo CXX=clang++ >> make\local
 if errorlevel 1 exit 1
-echo LINK=lld.exe >> make\local
+echo LD=lld.exe >> make\local
 if errorlevel 1 exit 1
 echo CPPFLAGS+=-D_REENTRANT -DBOOST_DISABLE_ASSERTS -D_BOOST_LGAMMA  >> make\local
 if errorlevel 1 exit 1
